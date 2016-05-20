863991874ceb2aa71106246aba60cf0c1a9671b9
Daniel Campbell <campbelldj@hotmail.com>
2016-05-20 13:04:42 +1200

    Updated to use an Expander to fold stepwise

--- expander/src/main.rs
+++ expander/src/main.rs
@@ -3,12 +3,14 @@ extern crate syntex_syntax as syntax;
 
 use syntax::ast;
 use syntax::ext::base::{ExtCtxt, SyntaxExtension};
-use syntax::ext::expand::ExpansionConfig;
+use syntax::ext::expand::{self, ExpansionConfig};
 use syntax::codemap::CodeMap;
 use syntax::errors::Handler;
 use syntax::errors::emitter::{ColorConfig};
+use syntax::fold::Folder;
 use syntax::parse::{self, ParseSess};
 use syntax::parse::token::intern;
+use syntax::ptr::P;
 
 use rustfmt::filemap::FileMap;
 use rustfmt::config::{Config, WriteMode};
@@ -104,9 +106,112 @@ fn expand_crate(data: &mut ExpandData) {
 
     //TODO: Can't expand builtins yet
     //syntax_ext::register_builtins(&mut ecx.syntax_env);
-    let expanded = syntax::ext::expand::expand_crate(ecx, Vec::new(), Vec::new(),
+    let expanded = expand::expand_crate(ecx, Vec::new(), Vec::new(),
                                                      data.krates[data.index].clone()).0;
 
     data.krates.push(expanded);
     data.index += 1; //Next expansion/write should happen on new crate
+}
+
+
+struct OnceExpander <'a, 'b:'a> {
+    pub cx: &'a mut ExtCtxt<'b>,
+}
+
+impl<'a, 'b> OnceExpander<'a, 'b> {
+    pub fn new(cx: &'a mut ExtCtxt<'b>) -> OnceExpander<'a, 'b> {
+        OnceExpander { cx: cx }
+    }
+}
+//TODO: Changed fold_expr, fold_pat, fold_stmt & fold_ty
+//TODO: (Still need to look at Blocks & Items)
+impl<'a, 'b> Folder for OnceExpander<'a, 'b> {
+    fn fold_crate(&mut self, c: Crate) -> Crate {
+        self.cx.filename = Some(self.cx.parse_sess.codemap().span_to_filename(c.span));
+        expand::noop_fold_crate(c, self)
+    }
+
+    fn fold_expr(&mut self, expr: P<ast::Expr>) -> P<ast::Expr> {
+        expand::expand_expr(expr, self, |_, expr| expr)
+    }
+
+    fn fold_pat(&mut self, pat: P<ast::Pat>) -> P<ast::Pat> {
+        expand::expand_pat(pat, self, |_, p| p)
+    }
+
+    fn fold_item(&mut self, item: P<ast::Item>) -> SmallVector<P<ast::Item>> {
+        use std::mem::replace;
+        let result;
+        if let ast::ItemKind::Mod(ast::Mod { inner, .. }) = item.node {
+            if item.span.contains(inner) {
+                self.push_mod_path(item.ident, &item.attrs);
+                result = expand::expand_item(item, self);
+                self.pop_mod_path();
+            } else {
+                let filename = if inner != codemap::DUMMY_SP {
+                    Some(self.cx.parse_sess.codemap().span_to_filename(inner))
+                } else { None };
+                let orig_filename = expand::replace(&mut self.cx.filename, filename);
+                let orig_mod_path_stack = expand::replace(&mut self.cx.mod_path_stack, Vec::new());
+                result = expand::expand_item(item, self);
+                self.cx.filename = orig_filename;
+                self.cx.mod_path_stack = orig_mod_path_stack;
+            }
+        } else {
+            result = expand::expand_item(item, self);
+        }
+        result
+    }
+
+    fn fold_item_kind(&mut self, item: ast::ItemKind) -> ast::ItemKind {
+        expand::expand_item_kind(item, self)
+    }
+
+    fn fold_stmt(&mut self, stmt: ast::Stmt) -> SmallVector<ast::Stmt> {
+        expand::expand_stmt(stmt, self, |_, s| s)
+    }
+
+    fn fold_block(&mut self, block: P<Block>) -> P<Block> {
+        let was_in_block = ::std::mem::replace(&mut self.cx.in_block, true);
+        let result = expand::expand_block(block, self);
+        self.cx.in_block = was_in_block;
+        result
+    }
+
+    fn fold_arm(&mut self, arm: ast::Arm) -> ast::Arm {
+        expand::expand_arm(arm, self)
+    }
+
+    fn fold_trait_item(&mut self, i: ast::TraitItem) -> SmallVector<ast::TraitItem> {
+        expand::expand_annotatable(Annotatable::TraitItem(P(i)), self)
+            .into_iter().map(|i| i.expect_trait_item()).collect()
+    }
+
+    fn fold_impl_item(&mut self, i: ast::ImplItem) -> SmallVector<ast::ImplItem> {
+        expand::expand_annotatable(Annotatable::ImplItem(P(i)), self)
+            .into_iter().map(|i| i.expect_impl_item()).collect()
+    }
+
+    fn fold_ty(&mut self, ty: P<ast::Ty>) -> P<ast::Ty> {
+        expand::expand_type(ty, self, |_, t| t)
+    }
+
+    fn new_span(&mut self, span: Span) -> Span {
+        expand::new_span(self.cx, span)
+    }
+}
+
+impl<'a, 'b> OnceExpander<'a, 'b> {
+    fn push_mod_path(&mut self, id: Ident, attrs: &[ast::Attribute]) {
+        let default_path = id.name.as_str();
+        let file_path = match ::attr::first_attr_value_str_by_name(attrs, "path") {
+            Some(d) => d,
+            None => default_path,
+        };
+        self.cx.mod_path_stack.push(file_path)
+    }
+
+    fn pop_mod_path(&mut self) {
+        self.cx.mod_path_stack.pop().unwrap();
+    }
 }
